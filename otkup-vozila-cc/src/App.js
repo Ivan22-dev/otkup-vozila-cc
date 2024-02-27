d96a8875951061729011e6415ca88a676e659197
Ivan22-dev <61786145+Ivan22-dev@users.noreply.github.com>
2024-02-27 12:01:13 +0100

    dialog, form, followbutton

--- otkup-vozila-cc/src/App.js
+++ otkup-vozila-cc/src/App.js
@@ -11,15 +11,21 @@ import "./theme.scss";
 
 //Import Icon Css
 import "./assets/css/materialdesignicons.min.css";
+import FormDialog from "./components/Modal/components/Dialog";
+
+
 
 const App = () => {
   return (
     <React.Fragment>
+      <FormDialog/>
       <Router>
         <Switch>
           {routes.map((route, idx) => (
             <Route path={route.path} component={route.component} key={idx} />
+            
           ))}
+       
         </Switch>
       </Router>
     </React.Fragment>
