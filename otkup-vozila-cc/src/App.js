ba0f42ff47fce9baf6a75f5e5d8ef69cb4753327
Ivan22-dev <61786145+Ivan22-dev@users.noreply.github.com>
2024-03-05 17:44:41 +0100

    ajme commit

--- otkup-vozila-cc/src/App.js
+++ otkup-vozila-cc/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import routes from "./routes";
 import {
   Route,
-  Switch,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Routes
 } from "react-router-dom";
 
 // Import Scss
@@ -11,21 +11,29 @@ import "./theme.scss";
 
 //Import Icon Css
 import "./assets/css/materialdesignicons.min.css";
+import Index1 from "./pages/Index1/Index1";
+const getRoutes = (allRoutes) =>
+allRoutes.map((route) => {
+  if (route.collapse) {
+    return getRoutes(route.collapse);
+  }
+
+  if (route.route) {
+    return <Route exact path={route.route} element={route.component} key={route.key} />;
+  }
+
+  return null;
+});
 
 
 
 const App = () => {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          {routes.map((route, idx) => (
-            <Route path={route.path} component={route.component} key={idx} />
-            
-          ))}
-       
-        </Switch>
-      </Router>
+       <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Index1 to="" />} />
+      </Routes>
     </React.Fragment>
   );
 };
